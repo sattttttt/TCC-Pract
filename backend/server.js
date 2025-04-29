ec85a41af221c96da5ed63710f1772fe6a1a9ed4
sattttttt <othmansatria80@gmail.com>
2025-04-29 22:20:42 +0700

    balikk

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,16 @@ import Express from "express";
 import route from "./routes/route.js";
 import Cors from "cors";
 
-const app = Express();
-const PORT = process.env.PORT || 5000;  // Tetap gunakan env.PORT
 
+
+const app = Express();
+
+const PORT =  5000;
 app.use(Cors());
 app.use(Express.json());
 app.use(route);
 
-app.get('/', (req, res) => {
-  res.json({ 
-    status: "Backend running",
-    port: PORT,
-    note: "Cloud Run automatically sets PORT env"
-  });
-});
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    console.log(`Server listening on port ${PORT}`);
+  });
