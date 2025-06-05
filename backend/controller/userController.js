3a21109d18f4245d0227ed9c83880c9f97fe5ebf
sattttttt <othmansatria80@gmail.com>
2025-06-05 16:06:52 +0700

    apdet baru1

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -35,7 +35,7 @@ const registerUsers = async (req, res) => {
     
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await Users.create({ name, email, password: hashedPassword });
+        const user = await User.create({ name, email, password: hashedPassword });
         res.status(201).json({ success: true, message: "User Registered Successfully", user });
     } catch (error) {
         res.status(500).json({ success: false, message: "Failed to register user", error });
